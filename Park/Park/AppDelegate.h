48e1f2a80ecb02d99cd612f269a0b6636bb2d8c9
jacob.jiang <jacob.jiang@ringcentral.com>
2015-07-01 13:13:54 +0800

    First import

---
+++ Park/Park/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Park
+//
+//  Created by jacob.jiang on 7/1/15.
+//  Copyright © 2015 Jacob Jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
